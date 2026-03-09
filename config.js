3e0e37c6903fadf06dfd476892cbb217b7275e36
timnasax2026 <timnasax@gmail.com>
2026-03-09 11:51:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "TIMNASA~WEKA_SESSION_HAPA",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "255xxxxxxxxx",
+    PREFIX: process.env.PREFIX || ".",
+    MODE: process.env.MODE || "public",
+    BOT_NAME: "TIMNASA NEXT FUTURE TMD",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+};
 
